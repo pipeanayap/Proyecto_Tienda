4d509081899624eef57dc94bcf45fd68f80e8bba
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2024-11-19 19:46:20 -0600

    infrastructure

---
+++ backend/variables.tf
@@ -0,0 +1,19 @@
+variable "DB_USER"{
+    type = string
+}
+
+variable "DB_PASSWORD"{
+    type = string
+}
+
+variable "DB_NAME"{
+    type = string
+}
+
+variable "DB_CLUSTER" {
+    type = string
+}
+
+variable "MONGO_URI" {
+    type = string
+}
